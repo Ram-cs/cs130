1c08dc8400e2f6cd328d5f7fb5e001aedd3fe3a3
Jesse Chen <jesseschchen@gmail.com>
2018-11-28 12:46:37 -0800

    ReplyController

--- cs130/cs130/ReplyController.swift
+++ cs130/cs130/ReplyController.swift
@@ -10,25 +10,35 @@ import UIKit
 
 class ReplyController: UIViewController {
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        
-        // create and setup navigation bar
         self.navigationController?.navigationBar.barTintColor = APP_BLUE
-        self.navigationController?.navigationBar.tintColor = UIColor.white
         self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navigationItem.title = "Reply"
+        navigationItem.title = "Create Reply"
+        view.backgroundColor = .white
+        let backButton = UIButton(type: .system);
+        backButton.setTitle("Back", for: .normal)
+        backButton.clipsToBounds = true
+        backButton.setTitleColor(UIColor.white, for: .normal)
+        backButton.titleLabel?.font=UIFont.systemFont(ofSize: 12)
+        backButton.isEnabled = true
+        let backButtonItem = UIBarButtonItem.init(customView: backButton)
+        navigationItem.leftBarButtonItem = backButtonItem
         
         // setup submit button
-        let submit = UIButton(type: .system);
-        submit.setTitle("Submit", for: .normal)
-        submit.isEnabled = true
-        submit.addTarget(self, action: #selector(submitAction), for: .touchUpInside)
-        let submitItem = UIBarButtonItem.init(customView: submit)
-        navigationItem.rightBarButtonItem = submitItem
+        //let submit = UIButton(type: .system);
+        //submit.setTitle("Submit", for: .normal)
+        //submit.isEnabled = true
+        //submit.addTarget(self, action: #selector(submitAction), for: .touchUpInside)
+        //let submitItem = UIBarButtonItem.init(customView: submit)
+        //navigationItem.rightBarButtonItem = submitItem
         
-        view.backgroundColor = .white
+        //view.backgroundColor = .white
         
+
+
         setUpReplyField()
+        createPostButton()
         replyField.becomeFirstResponder()
     }
     
@@ -42,6 +52,25 @@ class ReplyController: UIViewController {
         reply.font = UIFont.systemFont(ofSize: 16)
         return reply
     }()
+
+
+    let submitButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Post Comment", for: .normal)
+        button.layer.cornerRadius = 5
+        button.clipsToBounds = true
+        button.backgroundColor = UIColor.rgb(red: 17, green: 154, blue: 237)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.isEnabled = true
+        button.addTarget(self, action: #selector(submitHandle), for: .touchUpInside)
+        return button
+    }()
+
+    fileprivate func createPostButton(){
+        view.addSubview(submitButton)
+        submitButton.anchor(left: view.leftAnchor, leftPadding: 24, right: view.rightAnchor, rightPadding: -24, top: nil, topPadding: 0, bottom: view.bottomAnchor, bottomPadding: -70, width: 100, height: 40)
+    }
     
     // set constraints for replyfield
     // TODO: make replyfield take into account keyboard size (current constraints only work on iphone)
